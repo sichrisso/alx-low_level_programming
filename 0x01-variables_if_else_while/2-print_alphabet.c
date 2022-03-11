838720b94d52ef620af3000c0f5bcfc82fa14e70
sichrisso <helenasolomon87@gmail.com>
2022-03-11 21:49:49 +0300

    Prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Lanzador.
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	char n = 'a';
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
