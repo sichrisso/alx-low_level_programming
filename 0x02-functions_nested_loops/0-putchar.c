3720a4a01b81776d23b7e9c5fdd8ca83716ed4a6
sichrisso <helenasolomon87@gmail.com>
2022-03-15 10:44:09 +0300

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "putchar.h"
+
+/**
+* main - Lanzador
+*
+* Return: Always 0
+*
+*/
+
+int main(void)
+{
+	_putchar('P');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
