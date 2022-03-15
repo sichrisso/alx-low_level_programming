51b16e4fc5336c14fe5b7c2edd7d7d9922f9226c
sichrisso <helenasolomon87@gmail.com>
2022-03-15 18:05:02 +0300

    Tasks given on functions and nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "putchar.h"
+#include "holberton.h"
 
 /**
 * main - Lanzador
@@ -9,13 +9,15 @@
 
 int main(void)
 {
-	_putchar('P');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
 	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 	return (0);
 }
