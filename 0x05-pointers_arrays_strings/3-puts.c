32b248fd509ae818c59a12c753b60adaba147613
sichrisso <helenasolomon87@gmail.com>
2022-03-21 21:24:49 +0300

    Reverse

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts(char *str) - a function that prints a string, followed by a new line, to stdout
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    _puts(str);
+    return (0);
+}
