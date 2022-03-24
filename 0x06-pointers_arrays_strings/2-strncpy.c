52eac854e7aa445cf6f6a038d466a0bab8bb264c
sichrisso <helenasolomon87@gmail.com>
2022-03-24 09:24:25 +0300

    Copies a String

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,23 +1,26 @@
 #include "main.h"
+
 /**
-* *_strncpy - check the code for Holberton School students.
-* @dest : int
-* @src : int
-* @n : int
-* Return: Always 0.
-*/
+ * _strncpy - cpoies n bytes of src to the dest string
+ * @dest: string to copy to
+ * @src: string being copied
+ * @n: largest number of bytes to copy
+ *
+ * Return: address of dest
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i;
 
-	while (i < n && src[i] != '\0')
+	i = 0;
+	while (i < n && *(src + i))
 	{
-		dest[i] = src[i];
+		*(dest + i) = *(src + i);
 		i++;
 	}
 	while (i < n)
 	{
-		dest[i] = '\0';
+		*(dest + i) = '\0';
 		i++;
 	}
 	return (dest);
