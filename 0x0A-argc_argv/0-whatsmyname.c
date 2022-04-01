c412519f9a8b46d3528dd750627eb8c2e7e56c60
sichrisso <helenasolomon87@gmail.com>
2022-04-02 00:56:52 +0300

    task update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,17 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdio.h>
+
 /**
- * main - function that prints its name
- * @argc: count of the arguments
- * @argv: pointers array
- * Return: int
- */
-int main(int argc, char *argv[])
+* main - prints its name follwoed by a newline
+* @argc: number of arguments
+* @argv: the vector that hold the arguments
+*
+* Description: prints the current directory and the name like ./name
+* Return: returns the name.
+*/
+
+int main(int argc, char **argv)
 {
-	(void)argc;
-	printf("%s\n", *argv);
+	printf("%s\n", argv[argc - 1]);
 	return (0);
 }
