e771e1e2e9947012eb9cdfafc02f239fa8a4c611
sichrisso <helenasolomon87@gmail.com>
2022-04-05 08:28:28 +0300

    optional-task

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -7,44 +7,6 @@
  *
  * Return: Always 0.
  */
-#include <stdio.h>
-#include <stdlib.h>
-
-char *str_concat(char *, char *);
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-#include "holberton.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-#include <stdio.h>
-#include <stdlib.h>
-
-char *str_concat(char *, char *);
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-#include "holberton.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
 int main(void)
 {
     char *s;
