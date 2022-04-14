bca8e2c8f37f8fb4700662289fa8196583e75275
sichrisso <helenasolomon87@gmail.com>
2022-04-14 22:20:48 +0300

    update

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,17 +1,11 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
 
-#include <stdlib.h>
+#include <stddef.h>
 
-int _putchar(char c);
+int _putchar(char);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
-	
-#endif /* FUNCTION_POINTERS_H */
+
+#endif /* _FUNCTION_POINTERS_H_ */
