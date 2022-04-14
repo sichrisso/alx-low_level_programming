798e77dca462e5e0a3aa579dd8ca83ebaca17ff5
sichrisso <helenasolomon87@gmail.com>
2022-04-14 22:33:54 +0300

    update

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,31 +3,42 @@
 #include "3-calc.h"
 
 /**
- * main - performs simple operations
- * @argc: number of arguments passed
- * @argv: array of pointers to arguments
+ * main - Principal
  *
- * Return: always 0
+ * @ac: count arguments
+ * @ag: array
+ *
+ * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(int arc, char *arg[])
 {
-	int a, b, c;
-	int (*f)(int, int);
+	int num1, num2, (*operate)(int, int);
 
-	if (argc != 4)
+	if (arc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	f = get_op_func(argv[2]);
-	if (f == NULL)
+	if ((*arg[2] != '+' &&
+		*arg[2] != '-' &&
+		*arg[2] != '/' &&
+		*arg[2] != '*' &&
+		*arg[2] != '%')
+		|| arg[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	c = f(a, b);
-	printf("%d\n", c);
+
+	num1 = atoi(arg[1]);
+	num2 = atoi(arg[3]);
+	if ((*arg[2] == '/' || *arg[2] == '%') && (num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	operate = get_op_func(arg[2]);
+	printf("%d\n", operate(num1, num2));
 	return (0);
+
 }
