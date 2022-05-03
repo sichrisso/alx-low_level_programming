7dc596ae41917225a2139d9053af5ea199c597ac
sichrisso <helenasolomon87@gmail.com>
2022-05-03 12:49:04 +0300

    update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,5 +12,17 @@ int _putchar(char);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void check99(ssize_t check, char *file, int fd_from, int fd_to)
+int main(int argc, char *argv[])
+void check100(int check, int fd)
+void check97(int argc)
+void check98(ssize_t check, char *file, int fd_from, int fd_to)
+void read_error(char *argv)
+void check_elf(Elf64_Ehdr *header)
+void check_class(Elf64_Ehdr *header)
+void check_data_ver(Elf64_Ehdr *header)
+void check_type(Elf64_Ehdr *header)
+void check_entry(Elf64_Ehdr *header)
+
 
 #endif /* _MAIN_H_ */
